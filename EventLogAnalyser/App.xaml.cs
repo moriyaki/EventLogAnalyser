3f6e11127ea050717f86d8af93d2fbd8dc3bd4b0
moriyaki <moriyaki999@gmail.com>
2025-11-28 19:23:57 +0900

    プロジェクトファイルを追加。

---
+++ EventLogAnalyser/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
+using EventLogAnalyser.Services;
+using EventLogAnalyser.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventLogAnalyser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// サービスの登録をします
+        /// </summary>
+        public App()
+        {
+            Ioc.Default.ConfigureServices(Services);
+        }
+
+        /// <summary>
+        /// 現在の App インスタンスを取得します
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// サービスプロバイダです
+        /// </summary>
+        public IServiceProvider Services { get => ConfigureServices(); }
+
+        /// <summary>
+        /// サービスを登録します
+        /// </summary>
+        /// <returns></returns>
+        private static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IMessenger, WeakReferenceMessenger>();
+            services.AddSingleton<IMainViewModel, MainViewModel>();
+
+            services.AddSingleton<IEventLogService, EventLogService>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ EventLogAnalyser/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EventLogAnalyser.Dto;
+using EventLogAnalyser.Services;
+
+namespace EventLogAnalyser.ViewModels;
+
+public interface IMainViewModel;
+
+public class MainViewModel : ObservableObject, IMainViewModel
+{
+    private readonly IEventLogService _service;
+
+    public ObservableCollection<EventLogDto> Entries { get; } = [];
+
+    public MainViewModel(IEventLogService service)
+    {
+        _service = service;
+        Load();
+    }
+
+    private void Load()
+    {
+        Entries.Clear();
+        foreach (var item in _service.LoadSystemLog())
+        {
+            Entries.Add(item);
+        }
+    }
+}
